f354af84e94d1ef7af8ff521ec38e98cc022adb8
ucsdRgross <85006207+ucsdRgross@users.noreply.github.com>
2022-03-22 02:26:23 -0700

    changed player types for better physics

--- Godot/entities/player/Player.gd
+++ Godot/entities/player/Player.gd
@@ -1,7 +1,8 @@
-extends Area2D
+extends KinematicBody2D
 
 # Declare member variables here.
-export var speed = 400.0
+export (int) var speed = 200
+var velocity = Vector2()
 var screen_size = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
@@ -9,30 +10,25 @@ func _ready():
 	screen_size = get_viewport_rect().size
 	print(screen_size)
 
+#gets key presses
+#https://docs.godotengine.org/en/stable/tutorials/2d/2d_movement.html#:~:text=Click-and-move,-This%20last%20example&text=Clicking%20on%20the%20screen%20will,move%20to%20the%20target%20location.
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	#vector is (x,y)
-	var direction = Vector2.ZERO
-	#key presses
-	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("move_up"):
-		direction.y -= 1
-	if Input.is_action_pressed("move_down"):
-		direction.y += 1
-	
-	if direction.length() > 0:
-		#prevents moving faster diagonally
-		direction = direction.normalized()
-		$AnimatedSprite.play()
-	else:
-		$AnimatedSprite.stop()
-		$AnimatedSprite.frame = 0
-		
-	#delta is time since last frame, prevents moving faster with higher framerate and slower with lower framerate
-	position += direction * speed * delta
+func _physics_process(delta):
+	get_input()
+	velocity = move_and_slide(velocity)
+
 	#player cannot move beyond edge of screen
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
