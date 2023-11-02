98f24ed08ffe7485b7df9bbefa4341c50dd4a725
ucsdRgross <85006207+ucsdRgross@users.noreply.github.com>
2023-11-02 13:50:00 -0700

    shear works on xz axis now too

--- necromii/Components/shear.gd
+++ necromii/Components/shear.gd
@@ -20,11 +20,14 @@ func _physics_process(delta):
 		#this line of code specifically prevents values jumping when rotating by X, no idea why
 		var sY : float = s.y * cos(global_rotation.y)
 		shear.y.z = -shear_factor * 1.0/s.z * sY * abs(cos(global_rotation.y))
+		shear.x.z = -shear_factor * 1.0/s.z * sY * sin(global_rotation.z)
 		#removed shear affect if rotated by multiplying by zero
 		#shears and rotations dont mix very well
 		#X rotation very odd, acts like two different waves added together likely due to mixing with shear
-		var rot_factor = (1 - abs(sin(global_rotation.x*2))) * (1 - abs(cos(PI/2 + global_rotation.x))) * (cos(global_rotation.z))
-		shear.y.z *= rot_factor
+		var rot_factor_x = (1 - abs(sin(global_rotation.x*2))) * (1 - abs(cos(PI/2 + global_rotation.x)))
+		var rot_factor_z = cos(global_rotation.z)
+		shear.y.z *= rot_factor_x * rot_factor_z
+		shear.x.z *= rot_factor_x
 		child.basis = global_transform.basis * shear
 		
 func _on_child_entered_tree(node : Node3D):
