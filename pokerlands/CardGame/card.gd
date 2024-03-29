4485fd68a7318e765785741b6afea25d66bad47f
ucsdRgross <85006207+ucsdRgross@users.noreply.github.com>
2024-03-29 04:50:55 -0700

    mouse on card logic

--- pokerlands/CardGame/card.gd
+++ pokerlands/CardGame/card.gd
@@ -3,6 +3,8 @@ class_name Card
 
 var rank : int = 0
 var suit : int = 0
+var moused_hovered := false
+var dragging := false
 
 @onready var back_face: Sprite2D = $BackFace
 @onready var front_face: Sprite2D = $FrontFace
@@ -17,3 +19,16 @@ func show_front()  -> void:
 func show_back()  -> void:
 	front_face.hide()
 	back_face.show()
+
+func _on_mouse_button_1_pressed():
+	if moused_hovered:
+		dragging = true
+
+func _on_mouse_button_1_not_pressed():
+	dragging = false
+
+func _on_area_2d_mouse_entered():
+	moused_hovered = true
+
+func _on_area_2d_mouse_exited():
+	moused_hovered = false
--- pokerlands/CardGame/cardGameDemo.gd
+++ pokerlands/CardGame/cardGameDemo.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Node
 
 @export var PlayerScene : PackedScene
 @onready var card_player_1: Player = $CardPlayer1
@@ -16,7 +16,14 @@ func _ready():
 				$Label.text = str(GameManager.Players[i].index)
 				$camera.rotation_degrees = 180
 		index += 1
-
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == 1 and event.pressed:
+			get_tree().call_group("cards", "_on_mouse_button_1_pressed")
+		elif event.button_index == 1 and not event.pressed:
+			get_tree().call_group("cards", "_on_mouse_button_1_not_pressed")
