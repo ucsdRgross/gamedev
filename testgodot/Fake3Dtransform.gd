1593624ac3f349ead290cef9913b308d842772b4
ucsdRgross <85006207+ucsdRgross@users.noreply.github.com>
2024-04-30 20:54:37 -0700

    2D moving like 3D

---
+++ testgodot/Fake3Dtransform.gd
@@ -0,0 +1,10 @@
+@tool
+extends Node2D
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var node_3d: Node3D = $Node3D
+
+func _process(delta: float) -> void:
+	var b := node_3d.transform.basis
+	sprite_2d.transform.x = Vector2(b.x[0],b.x[1])
+	sprite_2d.transform.y = Vector2(b.y[0],b.y[1])
