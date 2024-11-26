152682bcbbba635d5a86050fb7c576934a540140
ucsdRgross <85006207+ucsdRgross@users.noreply.github.com>
2024-11-26 04:46:14 -0800

    able to convert image to lower res

--- testgodot/image_to_vectors.gd
+++ testgodot/image_to_vectors.gd
@@ -8,11 +8,18 @@ extends Control
 var img : Image
 
 func _process(_delta):
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+	if Input.is_action_just_pressed("rightclick"):
+		texture_rect.hide()
+		await get_tree().process_frame
 		img = paint_control.get_viewport().get_texture().get_image().get_region(Rect2(drawing_area.position, drawing_area.size))
+		texture_rect.show()
 		spread(get_global_mouse_position())
 		
 func spread(pos):
+	print(img.get_size())
+	var new_size = img.get_size() / 10
+	img.resize(new_size.x,new_size.y, 0)
+	print(img.get_size())
 	pos -= drawing_area.global_position 
 	var res : Vector2 = img.get_size()
 	var rel = pos/(drawing_area.size as Vector2) * res
