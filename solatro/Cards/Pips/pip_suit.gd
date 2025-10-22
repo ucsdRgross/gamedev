0fb65382436adefad86ac45b7a750dcf40c90a47
ucsdRgross <85006207+ucsdRgross@users.noreply.github.com>
2025-10-22 05:32:59 -0700

    Draft of pip classes design

---
+++ solatro/Cards/Pips/pip_suit.gd
@@ -0,0 +1,11 @@
+@abstract class_name PipSuit
+extends Resource
+
+@abstract func set_material(sprite:Sprite2D) -> void
+
+class Standard extends PipSuit:
+	var original_value : int
+	var value : int
+	func set_material(sprite:Sprite2D) -> void:
+		pass
+	
