d11368fcab73b3dfd3d586a978856bc71e4ebbab
ucsdRgross <85006207+ucsdRgross@users.noreply.github.com>
2026-03-21 11:27:32 -0700

    refactor CardDataIterator

--- solatro/Scripts/card_data_iterator.gd
+++ solatro/Scripts/card_data_iterator.gd
@@ -1,60 +1,24 @@
 class_name CardDataIterator
 
 var card_count : int
-var board : Array[ArrayCard]
 var next_card_data : CardData
-enum {DECK, INPUTS, BOARD, DISCARD, RULES}
-var phase := DECK
+enum {DECK, UPPER, LOWER, DISCARD, UPPER_RULES, LOWER_RULES, RULES}
+var phase : int = DECK
 
 func should_continue() -> bool:
 	match phase:
 		DECK:
-			if card_count < Game.CURRENT.draw_deck.size():
-				next_card_data = Game.CURRENT.draw_deck[card_count]
-				return true
-			else:
-				phase = INPUTS
-				card_count = 0
-				return should_continue()
-		INPUTS:
-			if not card_count < Game.CURRENT.inputs.size():
-				phase = BOARD
-				card_count = 0
-				return should_continue()
-			var card := Game.CURRENT.inputs[card_count].top_card
-			while not card:
-				card_count += 1
-				if not card_count < Game.CURRENT.inputs.size():
-					phase = BOARD
-					card_count = 0
-					return should_continue()
-				card = Game.CURRENT.inputs[card_count].top_card
-			next_card_data = card.data
-			return true
-		BOARD:
-			board = Game.CURRENT.get_board_cols()
-			if not board or not card_count < board[0].cards.size() * 5:
-				phase = DISCARD
-				card_count = 0
-				return should_continue()
-			var card : Card = board[card_count % 5].cards[card_count / 5]
-			while not card:
-				card_count += 1
-				if not card_count < board[0].cards.size() * 5:
-					phase = DISCARD
-					card_count = 0
-					return should_continue()
-				card = board[card_count % 5].cards[card_count / 5]
-			next_card_data = card.data
-			return true
+			return iterate_array(Game.CURRENT.draw_deck, UPPER)
+		UPPER:
+			return iterate_2d_array(Game.CURRENT.upper_zone, LOWER)
+		LOWER:
+			return iterate_2d_array(Game.CURRENT.lower_zone, DISCARD)
 		DISCARD:
-			if card_count < Game.CURRENT.discard_deck.size():
-				next_card_data = Game.CURRENT.discard_deck[card_count]
-				return true
-			else:
-				phase = RULES
-				card_count = 0
-				return should_continue()
+			return iterate_array(Game.CURRENT.discard_deck, UPPER_RULES)
+		UPPER_RULES:
+			return iterate_array(Game.CURRENT.upper_zone_type, LOWER_RULES)
+		LOWER_RULES:
+			return iterate_array(Game.CURRENT.lower_zone_type, RULES)
 		RULES:
 			if card_count < Game.CURRENT.rules_deck.size():
 				next_card_data = Game.CURRENT.rules_deck[card_count]
@@ -62,6 +26,36 @@ func should_continue() -> bool:
 			else:
 				return false
 	return false
+
+func iterate_array(deck:Array[CardData], next_stage:int) -> bool:
+	if card_count < deck.size():
+		next_card_data = deck[card_count]
+		return true
+	else:
+		phase = next_stage
+		card_count = 0
+		return should_continue()
+
+func iterate_2d_array(zone:Array[ArrayCardData], next_stage:int) -> bool:
+	var total_cards : int = 0
+	var largest_col : int = 0
+	for a in zone:
+		total_cards += a.datas.size()
+		if a.datas.size() > largest_col: largest_col = a.datas.size()
+	var count : int = 0
+	if card_count < total_cards:
+		for row : int in largest_col:
+			for col : int in zone.size():
+				if row < zone[col].datas.size():
+					if count == card_count:
+						next_card_data = zone[col].datas[row]
+						break
+					count += 1
+		return true
+	else:
+		phase = next_stage
+		card_count = 0
+		return should_continue()
 
 func _iter_init(arg:Variant) -> bool:
 	phase = DECK
